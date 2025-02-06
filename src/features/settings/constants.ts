7ef9e04c34b8ee9c82a230511fd14c2a7e814fd0
patrasambhit <patrasambhitkgp@gmail.com>
2025-02-07 00:47:26 +0530

    setting screen path & constants fixed

--- src/features/settings/constants.ts
+++
@@ -1,9 +0,0 @@
-export const STORAGE_KEYS = {
-  LANGUAGE: 'settings_language',
-  THEME: 'settings_theme',
-  FONT_SIZE: 'settings_font_size',
-  NOTIFICATIONS: 'settings_notifications',
-  PROFILE: 'user_profile',
-  REMINDERS: 'user_reminders',
-  EMERGENCY_CONTACTS: 'emergency_contacts'
-}; 
